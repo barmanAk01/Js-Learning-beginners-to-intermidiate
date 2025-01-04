d1125b3ce59a77769f744a63c71ff78ac711b73f
AK <rockshouter20@gmail.com>
2025-01-04 15:57:23 +0000

    conversions done

---
+++ Js_basics/conversion_operations.js
@@ -0,0 +1,34 @@
+let sec = 34;
+let lot= "23"
+// let toy = "12ac"
+// let toy = null
+let toy = undefined
+// let toy = true
+
+let valueInnumber= Number(toy)
+console.log(valueInnumber);
+console.log(typeof valueInnumber);
+
+//number => number
+//string => number
+//"12ac" => NaN ,number
+// null => 0, number
+// undefined => NaN, number
+// 
+
+
+// let booleanNum = Boolean(num)
+// console.log(booleanNum);
+
+
+
+// let num2= null
+// let stringNum = String(num2)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+
+
+
+
