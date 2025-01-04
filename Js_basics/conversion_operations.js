02c81cd6df3eff7f2d802f2fbee0e9842f6ad7b7
AK <rockshouter20@gmail.com>
2025-01-04 17:09:36 +0000

    done more chnages to operations

--- Js_basics/conversion_operations.js
+++ Js_basics/conversion_operations.js
@@ -1,24 +1,26 @@
-let sec = 34;
-let lot= "23"
-// let toy = "12ac"
-// let toy = null
-let toy = undefined
-// let toy = true
+// let sec = 34;
+// let lot= "23"
+// // let toy = "12ac"
+// // let toy = null
+// let toy = undefined
+// // let toy = true
 
-let valueInnumber= Number(toy)
-console.log(valueInnumber);
-console.log(typeof valueInnumber);
+// let valueInnumber= Number(toy)
+// console.log(valueInnumber);
+// console.log(typeof valueInnumber);
 
 //number => number
 //string => number
 //"12ac" => NaN ,number
 // null => 0, number
 // undefined => NaN, number
-// 
+// ------  conversions to number -------------
 
 
 // let booleanNum = Boolean(num)
 // console.log(booleanNum);
+// ------  conversions to boolean-------
+
 
 
 
@@ -26,8 +28,18 @@ console.log(typeof valueInnumber);
 // let stringNum = String(num2)
 // console.log(stringNum);
 // console.log(typeof stringNum);
+// ------  conversions to strings ------
 
 
+let x = 3;
+let y = x++;
+// let c = y++;
+
+console.log(x);
+console.log(y);
+// console.log(c);
+// value of x = 4
+// value of y = 3
 
 
 
