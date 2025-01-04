f9e1c376d968f5f10ecae9c83eb5b51501e3b039
AK <rockshouter20@gmail.com>
2025-01-04 17:34:23 +0000

    comparisions

---
+++ Js_basics/comparision.js
@@ -0,0 +1,17 @@
+// console.log("4" + " "+ 3);
+// console.log("4" > 3)
+
+let bool = true
+let boolNum = Number(bool)
+console.log(boolNum);
+console.log(typeof boolNum);
+
+console.log(true < 0) //expected -> false
+console.log(true > 0) //expected -> true
+
+console.log("                                  
+console.log();
+
+
+
+
