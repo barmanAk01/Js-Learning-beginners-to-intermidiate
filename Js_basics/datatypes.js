e17bdca79f79db86daa628a1d2728d18d775bc8f
AK <rockshouter20@gmail.com>
2025-01-04 12:11:56 +0000

    added datatypes

---
+++ Js_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //=> newer version of js 
+
+// alert(2+5)
+console.log(2+5); console.log("AK");
+
+//--> Primitive 
+// number -> 2 to power 53
+// bigint (more larger no than number)
+// string -> "...."
+// null -> standalone value
+// boolean -> true/false
+//undefined 
+//symbols -> unique
+
+//object
+
+console.log(typeof undefined);// output undefined 
+console.log(typeof null);// output object because null treated as object
---
+++ Js_basics/variables.js
@@ -0,0 +1,14 @@
+ const accountId = 243 
+ var accountEmail = "Blogger2@gamil.com"
+ let accountpass = "12345"
+ accountcity = "CRJ"
+
+//  accountId = 20
+ accountEmail="kal@mai.com"
+ accountpass= "123"
+ accountcity= "jmt"
+
+//  console.log(accountId);
+ console.table([accountId, accountEmail, accountpass, accountcity])
+
+ 
