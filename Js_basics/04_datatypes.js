38444be713070abbeb3a9f4609f71d069477ccca
AK <rockshouter20@gmail.com>
2025-01-06 21:34:45 +0000

    memory allocation

--- Js_basics/04_datatypes.js
+++ Js_basics/04_datatypes.js
@@ -1,16 +1,17 @@
 "use strict" //=> newer version of js 
 
 // alert(2+5)
-console.log(2+5); console.log("AK");
+// console.log(2+5); console.log("AK");
+
 
 //--> Primitive 
-// number -> 2 to power 53
+// number -> range -> 2 to power 53 
 // bigint (more larger no than number)
 // string -> "...."
 // null -> standalone value
 // boolean -> true/false
-//undefined 
-//symbols -> unique
+// undefined 
+// symbols -> unique
 
 //object
 
@@ -28,10 +29,12 @@ const newId = Symbol('620')
 // console.log(id === newId);
 // console.log(id == newId);
 
- console.log(typeof newId);
+//  console.log(typeof newId);
  
+
 //how to use bigInt
 const bigNumber = 9746189410471089n // just adding 'n' at the end of the number
+
 
 
 //Reference (Non-primitive)
@@ -39,18 +42,51 @@ const bigNumber = 9746189410471089n // just adding 'n' at the end of the number
 //Array, objects, functions all these non-primitive
 
 // use 
-const justname = ["kal" , "lol", "lal"];
-let myObj = {
-    language : "Typescript",
-    target : "UI",
-    proj : 2,
-}
+// const justname = ["kal" , "lol", "lal"];
+// let myObj = {
+//     language : "Typescript",
+//     target : "UI",
+//     proj : 2,
+// }
 
-const myFunction = function(){
-    console.log("Hey Ak");
+// const myFunction = function(){
+//     console.log("Hey Ak");
     
+// }
+
+// console.log( "justname is :" ,typeof justname);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
+ 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory allocations -
+//   1) stack() is used to primitive  
+//   2) heap() is used to non_primitive
+
+//example of stack memory-------------------- 
+let myName = "kalua"
+
+let anotherName = myName
+anotherName = "kal"
+
+console.log(anotherName);
+console.log(myName);
+//example of stack memory------------------ 
+
+
+//example of Heap memory--------------------
+let user1 ={
+    name: "RAHUL",
+    emial: "Rah@mail.com"
 }
 
-console.log( "justname is :" ,typeof justname);
-console.log(typeof myObj);
-console.log(typeof myFunction);
+// Now I will just change the value of email in user2 , bt this will also change the value in user1 due to same point of reference.
+let user2 = user1; 
+
+ user2.emial ="Rahul072mail.com"
+ console.log("value of user1 : ",user1);
+ console.log("value of user2 : ",user2);
+//example of Heap memory--------------------
+ 
+ 
