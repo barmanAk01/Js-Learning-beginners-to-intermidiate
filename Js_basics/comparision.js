6cc9fdf81f8e845390ab1293b3860972f62ad03b
AK <rockshouter20@gmail.com>
2025-01-06 18:12:10 +0000

    comparision done

--- Js_basics/comparision.js
+++ Js_basics/comparision.js
@@ -1,17 +1,22 @@
 // console.log("4" + " "+ 3);
 // console.log("4" > 3)
 
-let bool = true
-let boolNum = Number(bool)
-console.log(boolNum);
-console.log(typeof boolNum);
+// let bool = true
+// let boolNum = Number(bool)
+// console.log(boolNum);
+// console.log(typeof boolNum);
 
-console.log(true < 0) //expected -> false
-console.log(true > 0) //expected -> true
+// console.log(true < 0) //expected -> false
+// console.log(true > 0) //expected -> true
 
-console.log("                                  
-console.log();
+console.log(null == 0); // used to check any types 
+console.log("3" == 3)   // true
 
+console.log("3" === 3)  //false                                   
+console.log( 3 === 3);  //true
+console.log("3" === "3")//true
 
+// strictlyEqual check(===)
+// === -> check the types 1 string and 1 number it will return false
 
 
