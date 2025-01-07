682ba6d422a4b03f1bdcf825b655eead5ef671a7
AK <rockshouter20@gmail.com>
2025-01-07 12:06:44 +0000

    added Strings fn()s

---
+++ Js_basics/06_strings.js
@@ -0,0 +1,36 @@
+//Dont use this syntax for string 
+const name = "gourav"
+const age = 25
+
+// console.log(name + age );
+console.log( typeof name);
+
+
+// Instead now a days use like this inject the value directly
+// console.log(`My name is ${name} and my age is ${age} `);
+ 
+
+// let tName = new String('kal');
+// console.log( typeof tName);
+// console.log(tName.toUpperCase());
+// console.log(tName);
+
+// const num = 200;
+// console.log(num);
+
+// const balance = new Number(200)
+// console.log(balance);
+// console.log(balance.toFixed(3));// toFixed() is used mostly to e-commerce sites for more presice amount in bills.
+
+
+ const letNum = 234.56534
+//  console.log(letNum.toPrecision(2));
+ 
+
+ const price = 60000000
+//  console.log(price.toLocaleString());  // convert to US standard
+//  console.log(price.toLocaleString('EN-IN')); // convert to indian standard
+
+ console.log(Math.random());
+ 
+
