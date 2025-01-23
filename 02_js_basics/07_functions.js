14bc2a2992c5d38d6318e7d86f6db4354937c963
AK <rockshouter20@gmail.com>
2025-01-23 10:51:51 +0000

    Added function files

--- 02_js_basics/07_functions.js
+++ 02_js_basics/07_functions.js
@@ -1,5 +1,13 @@
-
-function myCartItem(prices){   
-    return prices
+// I have to show my all item prices of cart 
+// bt this syntax i m able to print only 1 i.e. 200 nt all  
+function cart1(prices01){   
+    return prices01
 }      
-myCartItem(200, 300, 500)
+ console.log( "cart_1 : ",cart1 (200, 300, 500) ); // print only 200
+  
+
+ // To get all the item prices, to solve this case we use rest operator => ' ... '
+ function cart2(...prices02){   
+    return prices02
+}      
+ console.log( "cart_2: ",cart2 (200, 300, 500) ); 
