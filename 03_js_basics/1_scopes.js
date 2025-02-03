5927ece0046eac967a26928090504275baef2dcf
AK <92323484+GITAK007@users.noreply.github.com>
2025-02-03 21:26:00 +0530

    correction

--- 03_js_basics/1_scopes.js
+++ 03_js_basics/1_scopes.js
@@ -1,13 +1,20 @@
-// By using 'Var' this we can redeclare the same variable name with different values 
+/*
+  Global Scope : Avoid overuse to prevent conflicts.
+  Function Scope :  Use var carefully (it’s function-scoped, not block-scoped).
+  Block Scope :  Prefer let/const for tighter control.
+  Module Scope : Use ES6 modules for organized, scalable code.
+  Closures : Enable powerful patterns like memoization and encapsulation.
+*/
+
+// By using 'Var' , we can declare same variable name with different values 
 let val_1 = 23
 
 
-
-// By using 'let' this we can only modify with different values 
+// By using 'let', we can only modify with different values 
 const  val_2 = 21
 
 
-// By using 'const' this we cannot modify values neither redeclare the same variable name
+// By using 'const', we cannot modify values neither redeclare the same variable name
 var val_3 = 25
 // value = 16 // NOT ALLOWED
 
@@ -15,18 +22,16 @@ if (true){
     let val_1 = 24
     const val_2 = 25
     var val_3 = 29
-    console.log("Inner val_1 -> ",val_1);
+    console.log("Inner val_1 -> ",val_1); 
     
 }
-console.log("Global scope values :  ");
-console.log( val_1 );
+
+// console.log("Global scope values :  ");
+
+// console.log( val_1 );
+// console.log( val_2);
+// console.log( val_3);
 
 
-console.log(val_2);
 
 
-console.log(val_3);
-
-
-// Added something
-// please check this able to commit in my github 
